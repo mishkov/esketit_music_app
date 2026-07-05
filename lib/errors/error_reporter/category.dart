0daf92608eded00ed11f9a2808bcbd851e0e1256
Nikita Mishkov <mishkov.dd@gmail.com>
2026-07-05 23:02:37 +0300

    Fix unused enum value

--- lib/errors/error_reporter/category.dart
+++ lib/errors/error_reporter/category.dart
@@ -2,10 +2,6 @@ enum Category {
   generic,
   uiClick,
   uiInput,
-
-  // TODO: remove this value because it's copied from app which is in another domain.
-  stateHandler,
-
   http,
   navigation,
 }
