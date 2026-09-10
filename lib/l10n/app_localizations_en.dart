d284b16c27e42e0c056fb7a5bf58c3ed7c4a2bf2
Nikita Mishkov <mishkov.dd@example.com>
2026-09-10 23:20:27 +0300

    fix: remove show more buttons

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -508,9 +508,6 @@ class AppLocalizationsEn extends AppLocalizations {
   String get lastAddedTracksTitle => 'Last added';
 
   @override
-  String get viewMoreButton => 'View more';
-
-  @override
   String get noTracksYet => 'No tracks yet.';
 
   @override
